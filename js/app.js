b987a6d833852523cffbe9cf0b40a9c00eeaccd8
kadeembestteaches <56241232+kadeembestteaches@users.noreply.github.com>
2020-12-12 12:18:56 -0500

    Application completed without the left and right arrow

---
+++ js/app.js
@@ -0,0 +1,38 @@
+
+
+function main()
+{
+
+    
+       //Get h1
+        const heading =  document.querySelector("h1");
+        
+        //Get all the buttons
+        const buttons = document.querySelectorAll("button");// will always return an array
+
+        //This will allow me to set an event to the first element in the array
+        buttons[0].addEventListener("click",function(){
+
+           heading.style.color = "#ffff00";
+        })
+
+        buttons[1].addEventListener("click",function(){
+
+            heading.style.color ="#00ff00";
+        })
+
+        buttons[2].addEventListener("click",function(){
+
+            heading.style.color="#000000";
+        })
+
+        buttons[3].addEventListener("click",function(){
+
+            heading.innerText ="DOM Bacchanal";
+
+        })
+    
+}
+
+
+main();
